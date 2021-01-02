7e5183d9ac99aaa56776b8626a6a7ef4588438c9
Jason Godmere <jgodmere@UX550VE-laptop.attlocal.net>
2021-01-01 23:53:23 -0500

    basic login request to Horus

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 
+'''
 import asyncio
 import websockets
 import platform
 import json
 
 async def hello():
-	uri = 'ws://localhost:8000/ws/'
+	uri = 'ws://localhost:8000/ws/hub/'
 
 	uname = platform.uname()
 
 	system_info = [
 		f"System: {uname.system}",
-		f"Node Name: {uname.node}",
+		f"Node Name: {uname.node.split('.', 1)[0]}",
 		f"Release: {uname.release}",
 		f"Version: {uname.version}",
 		f"Machine: {uname.machine}",
 		f"Processor: {uname.processor}"
 	]
 
+	data = {
+		'system_info': system_info,
+		'token': 'AYYYY its a node'
+	}
+
 	async with websockets.connect(uri) as websocket:
 
-		await websocket.send(json.dumps(system_info))
+		await websocket.send(json.dumps(data))
 		print('\n' + ("="*50))
 		print("System Info")
 		for item in system_info:
@@ -31,3 +37,12 @@ async def hello():
 
 
 asyncio.get_event_loop().run_until_complete(hello())
+'''
+
+import json
+import requests
+
+
+request = requests.get('http://127.0.0.1:8000/eoh/initnode/', auth=('user', 'pass'))
+
+print(request.json())
